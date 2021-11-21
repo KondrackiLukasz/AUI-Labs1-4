9abb79c6d847d522461fbfa012f9182dc8851f07
LukaszKond <potrix3@gmail.com>
2021-11-21 01:29:11 +0100

    API gateway microservice v1.0

--- src/main/java/com/example/demo/AuiL1Application.java
+++ src/main/java/com/example/demo/AuiL1Application.java
@@ -2,6 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class AuiL1Application {
@@ -10,4 +19,35 @@ public class AuiL1Application {
 		SpringApplication.run(AuiL1Application.class, args);
 	}
 
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder
+				.routes()
+				.route("militaryUnits", r -> r
+						.host("localhost:8080")
+						.and()
+						.path("/api/militaryUnits/{name}", "/api/militaryUnits")
+						.uri("http://localhost:8081"))
+				.route("soldiers", r -> r
+						.host("localhost:8080")
+						.and()
+						.path("/api/soldiers", "/api/soldiers/**", "/api/militaryUnits/{name}/soldiers", "/api/militaryUnits/{name}/soldiers/**")
+						.uri("http://localhost:8082"))
+				.build();
+	}
+
+	@Bean
+	public CorsWebFilter corsWebFilter() {
+
+		final CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.setAllowedOrigins(Collections.singletonList("*"));
+		corsConfig.setMaxAge(3600L);
+		corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT"));
+		corsConfig.addAllowedHeader("*");
+
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+
+		return new CorsWebFilter(source);
+	}
 }
